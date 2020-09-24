994ef1f662696e17296d84eadf6ef24c9b14cac1
azis <abdlazis530@gmail.com>
2020-09-24 10:59:52 +0700

    server 100%

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -1,15 +1,23 @@
 require('dotenv').config()
 const secret = process.env.ACCESS_TOKEN_SECRET
+const jwt = require('jsonwebtoken')
+
 function authenticateToken(req, res, next) {
 
-  const authHeader = req.headers['Authorization']
+  const authHeader = req.header('Authorization')
+
   const token = authHeader && authHeader.split(' ')[1]
+
+  console.log(secret)
   if (token == null) return res.sendStatus(401)
 
   jwt.verify(token, secret, (err, user) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.user = user
+ 
+    if (err) {
+        console.log(err)
+        return res.sendStatus(403)
+    }
+   
     next()
   })
 }
